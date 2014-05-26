6f9eb279a4d7d5077cdfd8885d5207b4ccddec80
Jake Nations <jnations1214@gmail.com>
2014-05-26 14:45:45 -0500

    basic deque completed

--- basic/deque/deque.py
+++ basic/deque/deque.py
@@ -5,4 +5,18 @@ class Deque:
     def isEmpty(self):
         return self.items == []
 
-    
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0,item)
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
