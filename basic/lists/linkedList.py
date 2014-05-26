b07606fa49bcffebd681ebb87f1cfd9114d3ddeb
Jake Nations <jnations1214@gmail.com>
2014-05-26 16:47:32 -0500

    linked list completed

--- basic/lists/linkedList.py
+++ basic/lists/linkedList.py
@@ -15,4 +15,66 @@ class Node:
     def setNext(self, newnext):
         self.next = newnext
 
+class UnorderedList:
+    def __init__(self):
+        self.head = None
 
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        node = Node(item)
+        node.setNext(self.head)
+        self.head = node
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else: 
+                current = current.getNext()
+
+        return found
+
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+newList = UnorderedList()
+newList.add(3)
+newList.add(5)
+newList.add(100)
+newList.add(67)
+newList.add(43)
+newList.add(89)
+newList.add(123643456)
+newList.add(78)
+newList.add(9012465)
+print newList.search(43)
+print newList.size()
+newList.remove(43)
+print newList.size()
+print newList.search(43)
